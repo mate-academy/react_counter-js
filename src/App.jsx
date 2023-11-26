f590ce9d3ab0b3319a70421a8b746d858926a8f7
olegnovikov10 <relaxinfh071@gmail.com>
2023-11-26 16:33:11 +0200

    task done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    setCount(prev => prev + 1);
   };
 
   const add100 = () => {
-    // write code here
+    setCount(prev => prev + 100);
   };
 
   // DON'T change the code below
