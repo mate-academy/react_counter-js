05663f8ce7c14a9858d7f54a49caeb4c9ae50e4a
Vasya564 <dudlavasil@gmail.com>
2024-02-21 16:28:26 +0200

    Add solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+/* eslint-disable no-shadow */
 import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    setCount(count => count + 1);
   };
 
   const add100 = () => {
-    // write code here
+    setCount(count => count + 100);
   };
 
   // DON'T change the code below
