dba46cf06bcf8308ffa8bb06422c2639e40a7819
Andrey <galosaandrew@gamil.com>
2024-03-13 14:12:17 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ export const App = () => {
     setCount(newCount => newCount + 100);
   };
 
-  // DON'T change the code below
   const increase = () => {
     if (count % 5 === 0) {
       add100();
@@ -25,27 +24,15 @@ export const App = () => {
     <div className="App">
       <h1 className="App__title">{`Count: ${count}`}</h1>
 
-      <button
-        type="button"
-        className="App__add-one"
-        onClick={() => addOne(count)}
-      >
+      <button type="button" className="App__add-one" onClick={addOne}>
         Add 1
       </button>
 
-      <button
-        type="button"
-        className="App__add-100"
-        onClick={() => add100(count)}
-      >
+      <button type="button" className="App__add-100" onClick={add100}>
         Add 100
       </button>
 
-      <button
-        type="button"
-        className="App__increase"
-        onClick={() => increase(count)}
-      >
+      <button type="button" className="App__increase" onClick={increase}>
         Increase
       </button>
     </div>
