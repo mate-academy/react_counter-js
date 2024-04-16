77d37b56d8c78f011da9b2b386eedba7c1521440
Rafael Wesley <rafaelwesleylima@gmail.com>
2024-04-16 14:32:49 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  let countIn = count;
+
 
   const addOne = () => {
-    // write code here
+    countIn++;
+    setCount(countIn);
   };
 
   const add100 = () => {
-    // write code here
-  };
+    countIn += 100;
+    setCount(countIn);
+  };  
 
   // DON'T change the code below
   const increase = () => {
+    
     if (count % 5 === 0) {
       add100();
     }
@@ -22,20 +27,22 @@ export const App = () => {
   };
 
   return (
-    <div className="App">
-      <h1 className="App__title">{`Count: ${count}`}</h1>
+    <>
+      <div className="App">
+        <h1 className="App__title">{`Count: ${count}`}</h1>
 
-      <button type="button" className="App__add-one" onClick={addOne}>
-        Add 1
-      </button>
+        <button type="button" className="App__add-one" onClick={addOne}>
+          Add 1
+        </button>
 
-      <button type="button" className="App__add-100" onClick={add100}>
-        Add 100
-      </button>
+        <button type="button" className="App__add-100" onClick={add100}>
+          Add 100
+        </button>
 
-      <button type="button" className="App__increase" onClick={increase}>
-        Increase
-      </button>
-    </div>
+        <button type="button" className="App__increase" onClick={increase}>
+          Increase
+        </button>
+      </div>
+    </>
   );
 };
