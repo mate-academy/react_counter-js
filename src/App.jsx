8bbfa64d2b43ec14a39461ff29e09a2f06a7adf3
Dmytro Koriahin <Dkoryahin@gmail.com>
2024-04-23 23:14:43 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  // eslint-disable-next-line prefer-const
+  let [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    setCount(() => (count += 1));
   };
 
   const add100 = () => {
-    // write code here
+    setCount(() => (count += 100));
   };
 
   // DON'T change the code below
