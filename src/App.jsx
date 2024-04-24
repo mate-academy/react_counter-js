d0a2e1310d4b531742e41a8fbb9cffc400bef04c
Vlad <fantom2569@gmail.com>
2024-04-24 15:06:51 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,21 @@ import './App.scss';
 
 export const App = () => {
   const [count, setCount] = useState(0);
-  const [shouldAdd100, setShouldAdd100] = useState(false);
 
   const addOne = () => {
-    setCount(count + 1);
+    setCount(prevCount => prevCount + 1);
   };
 
   const add100 = () => {
-    setCount(count + 100);
+    setCount(prevCount => prevCount + 100);
   };
 
   const increase = () => {
-    if (shouldAdd100) {
+    if (count % 5 === 0) {
       add100();
-      setShouldAdd100(false);
-    } else {
-      addOne();
-      if ((count + 1) % 5 === 0) {
-        setShouldAdd100(true);
-      }
     }
+
+    addOne();
   };
 
   return (
