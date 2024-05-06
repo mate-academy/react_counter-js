c050e094895c097aa334d8e28d215be4ef6d60ea
Oleksandr Poviakalo <oleksandr.poviakalo@gmail.com>
2024-05-06 14:43:28 +0300

    Made task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    setCount(prev => prev + 1);
   };
 
   const add100 = () => {
-    // write code here
+    setCount(prev => prev + 100);
   };
 
   // DON'T change the code below
@@ -22,18 +22,18 @@ export const App = () => {
   };
 
   return (
-    <div className="App">
-      <h1 className="App__title">{`Count: ${count}`}</h1>
+    <div className="app">
+      <h1 className="app__title">{`Count: ${count}`}</h1>
 
-      <button type="button" className="App__add-one" onClick={addOne}>
+      <button type="button" className="app__add-one" onClick={addOne}>
         Add 1
       </button>
 
-      <button type="button" className="App__add-100" onClick={add100}>
+      <button type="button" className="app__add-100" onClick={add100}>
         Add 100
       </button>
 
-      <button type="button" className="App__increase" onClick={increase}>
+      <button type="button" className="app__increase" onClick={increase}>
         Increase
       </button>
     </div>
