37065fed6108e4a5e0966d873e3d32a9850b352a
Oleksandr Poviakalo <oleksandr.poviakalo@gmail.com>
2024-05-06 22:11:59 +0300

    Changed className from app to  App

--- src/App.jsx
+++ src/App.jsx
@@ -22,18 +22,18 @@ export const App = () => {
   };
 
   return (
-    <div className="app">
-      <h1 className="app__title">{`Count: ${count}`}</h1>
+    <div className="App">
+      <h1 className="App__title">{`Count: ${count}`}</h1>
 
-      <button type="button" className="app__add-one" onClick={addOne}>
+      <button type="button" className="App__add-one" onClick={addOne}>
         Add 1
       </button>
 
-      <button type="button" className="app__add-100" onClick={add100}>
+      <button type="button" className="App__add-100" onClick={add100}>
         Add 100
       </button>
 
-      <button type="button" className="app__increase" onClick={increase}>
+      <button type="button" className="App__increase" onClick={increase}>
         Increase
       </button>
     </div>
