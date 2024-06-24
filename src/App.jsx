9899d8362746a2522e8812e541f6397016a68d05
Yaroslav Leshchenko <yaroslavleshenkoforwork@gmail.com>
2024-06-24 23:40:57 +0300

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    return setCount(count + 1)
   };
 
   const add100 = () => {
-    // write code here
+    return setCount(count + 100)
   };
 
   // DON'T change the code below
   const increase = () => {
     if (count % 5 === 0) {
-      add100();
+      return setCount(count + 101);
     }
 
     addOne();
