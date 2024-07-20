0f198d90796c77197276be86ec4b1962147debf1
Alexandr Shatskyy <autome@ukr.net>
2024-07-20 13:55:51 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  let sum = count;
 
   const addOne = () => {
-    // write code here
+    sum += 1;
+
+    return setCount(sum);
   };
 
   const add100 = () => {
-    // write code here
+    sum += 100;
+
+    return setCount(sum);
   };
 
-  // DON'T change the code below
   const increase = () => {
     if (count % 5 === 0) {
       add100();
