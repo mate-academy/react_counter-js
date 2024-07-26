6086b3ff81c08488bc8a3413fbe88cdf5a93253a
pryslipskyi.d <denys.pryslipskyi@nixs.com>
2024-07-26 16:03:41 +0200

    sol

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    setCount(prev => {
+      return prev + 1;
+    });
   };
 
   const add100 = () => {
-    // write code here
+    setCount(prev => {
+      return prev + 100;
+    });
   };
 
-  // DON'T change the code below
   const increase = () => {
     if (count % 5 === 0) {
       add100();
