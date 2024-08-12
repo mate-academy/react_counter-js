e965fb6dd1bf6305a79a7399a0ea242d8da129ce
Sasha Stopochkin <stopochkin23@gmail.com>
2024-08-12 14:23:16 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ export const App = () => {
   const [count, setCount] = useState(0);
 
   const addOne = () => {
-    setCount(count => count + 1);
+    setCount(a => a + 1);
   };
 
   const add100 = () => {
-    setCount(count => count + 100);
+    setCount(b => b + 100);
   };
 
   // DON'T change the code below
