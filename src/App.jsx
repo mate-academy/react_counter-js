63d78e90ff0acf49a6ef4c5ce054835ab0f052d0
Virineya Marchuk <marchukvirineya@gmail.com>
2024-10-18 14:04:02 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,28 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    setCount(currentCount => {
+      let newCount = currentCount;
+
+      newCount += 1;
+
+      return newCount;
+    });
   };
 
   const add100 = () => {
-    // write code here
+    setCount(currentCount => {
+      let newCount = currentCount;
+
+      newCount += 100;
+
+      return newCount;
+    });
   };
 
-  // DON'T change the code below
   const increase = () => {
     if (count % 5 === 0) {
       add100();
@@ -25,15 +36,27 @@ export const App = () => {
     <div className="App">
       <h1 className="App__title">{`Count: ${count}`}</h1>
 
-      <button type="button" className="App__add-one" onClick={addOne}>
+      <button
+        type="button"
+        className="App__add-one"
+        onClick={() => addOne(count)}
+      >
         Add 1
       </button>
 
-      <button type="button" className="App__add-100" onClick={add100}>
+      <button
+        type="button"
+        className="App__add-100"
+        onClick={() => add100(count)}
+      >
         Add 100
       </button>
 
-      <button type="button" className="App__increase" onClick={increase}>
+      <button
+        type="button"
+        className="App__increase"
+        onClick={() => increase(count)}
+      >
         Increase
       </button>
     </div>
