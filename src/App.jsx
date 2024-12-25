100af5ab12959c90e1a071c1af36e151416ff64d
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2024-12-25 08:33:27 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    setCount(currentCount => {
+      return currentCount + 1;
+    });
   };
 
   const add100 = () => {
-    // write code here
+    for (let i = 0; i < 100; i += 1) {
+      addOne();
+    }
   };
 
   // DON'T change the code below
