e763116070ee74ae3378403783f10c4b7b2d347a
Vasyl <priymak17ukrvaha@gmail.com>
2025-03-16 17:20:57 +0200

    rendered code

--- src/App.jsx
+++ src/App.jsx
@@ -4,56 +4,36 @@ import './App.scss';
 export const App = () => {
   const [count, setCurrentCount] = useState(0);
 
-  const addOne = currentCount => {
-    let newCurrentCount = currentCount;
-
-    newCurrentCount += 1;
-
-    setCurrentCount(newCurrentCount);
+  const addOne = () => {
+    setCurrentCount(prevCount => prevCount + 1);
   };
 
-  const add100 = currentCount => {
-    let newCurrentCount = currentCount;
-
-    newCurrentCount += 100;
-
-    setCurrentCount(newCurrentCount);
+  const add100 = () => {
+    setCurrentCount(prevCount => prevCount + 100);
   };
 
   // DON'T change the code below
-  const increase = currentCount => {
-    if (currentCount % 5 === 0) {
-      add100(currentCount + 1);
-    } else {
-      addOne(currentCount);
+  const increase = () => {
+    if (count % 5 === 0) {
+      add100();
     }
+
+    addOne();
   };
 
   return (
     <div className="App">
       <h1 className="App__title">{`Count: ${count}`}</h1>
 
-      <button
-        type="button"
-        className="App__add-one"
-        onClick={() => addOne(count)}
-      >
+      <button type="button" className="App__add-one" onClick={addOne}>
         Add 1
       </button>
 
-      <button
-        type="button"
-        className="App__add-100"
-        onClick={() => add100(count)}
-      >
+      <button type="button" className="App__add-100" onClick={add100}>
         Add 100
       </button>
 
-      <button
-        type="button"
-        className="App__increase"
-        onClick={() => increase(count)}
-      >
+      <button type="button" className="App__increase" onClick={increase}>
         Increase
       </button>
     </div>
